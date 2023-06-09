b855551e2bf2767f6f086e79ef7696926c3d3abe
Andrei Smirnov <andrei.smirnov.69@gmail.com>
2023-06-09 01:39:03 -0400

    initial python module commit

---
+++ examples/scripts/t.py
@@ -0,0 +1,5 @@
+import vrdf
+
+if __name__ == "__main__":
+    vrdf.hello()
+    
---
+++ setup.py
@@ -0,0 +1,17 @@
+# using https://stackoverflow.com/a/55266233/1181482 as starting point
+
+from setuptools import setup, find_packages
+
+setup(
+    name="vrdf",
+    version="0.0",
+    packages=find_packages(),
+    install_requires=[
+        "rdflib",
+        "graphviz",
+        "bs4",
+        "lxml",
+        "plotly",
+        "kaleido",
+    ],
+)
