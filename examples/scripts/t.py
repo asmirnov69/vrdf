03a8d2f03320a5b8d730006c170fa0c93e28c7ff
Andrei Smirnov <andrei.smirnov.69@gmail.com>
2023-06-09 02:18:03 -0400

    added working alice-bob example with shacl schema

--- examples/scripts/t.py
+++ examples/scripts/t.py
@@ -1,5 +1,7 @@
 import vrdf
 
 if __name__ == "__main__":
-    vrdf.hello()
+    g = vrdf.load_rdf(["./alice-bob.ttl", "./alice-bob-shacl.ttl"g])
+    print([spo for spo in g])
     
+
--- vrdf/__init__.py
+++ vrdf/__init__.py
@@ -1,3 +1,17 @@
-def hello():
-    print("Hello, world!")
+import rdflib
+
+def load_rdf(rdf_fn):
+    if isinstance(rdf_fn, str):
+        rdf_fns = [rdf_fn]
+    elif isinstance(rdf_fn, list):
+        rdf_fns = rdf_fn
+    else:
+        raise Exception("rdf_fn should be either str or list")
+    
+    g = rdflib.Graph()
+    for rdf_fn in rdf_fns:
+        with open(rdf_fn, "r") as fd:
+            g.parse(fd)
+
+    return g
     
